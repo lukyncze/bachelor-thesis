7ffff66be73987e4c12d186ea8ca1daf0a061dea
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2023-11-17 16:02:13 +0100

    feat(svelte): counter app

---
+++ apps/svelte/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <h1>Svelte Landing Page</h1>
+
+  <Counter />
+</main>
---
+++ apps/svelte/src/lib/Button.svelte
@@ -0,0 +1,10 @@
+
+<script lang="ts">
+  export let className: string;
+  export let onClick: () => void;
+  export let textContent: string;
+</script>
+
+<button class="px-4 py-2 rounded-md focus:outline-none {className}" on:click={onClick}>
+  {textContent}
+</button>
