048b38b8d4713b87cccbdc78433b37a1d79c2691
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2023-11-17 22:06:58 +0100

    update(svelte): config + observations

--- apps/svelte/tailwind.config.cjs
+++ apps/svelte/tailwind.config.cjs
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-/** @type {import('tailwindcss').Config}*/
 export default {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-
+  content: [
+    "./src/**/*.{svelte,js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-
   plugins: [],
 };
 
