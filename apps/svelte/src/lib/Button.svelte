65112022c9c74f89594c3c383c3a40b9b3fb0130
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2023-11-19 14:29:26 +0100

    update(svelte): use slot, prettier

--- apps/svelte/src/lib/Button.svelte
+++ apps/svelte/src/lib/Button.svelte
@@ -1,10 +1,8 @@
-
 <script lang="ts">
   export let className: string;
   export let onClick: () => void;
-  export let textContent: string;
 </script>
 
 <button class="px-4 py-2 rounded-md focus:outline-none {className}" on:click={onClick}>
-  {textContent}
+  <slot />
 </button>
--- apps/svelte/src/lib/Counter.svelte
+++ apps/svelte/src/lib/Counter.svelte
@@ -1,30 +1,25 @@
 <script lang="ts">
   import Button from './Button.svelte';
-  
+
   let count = 0;
 
-  const increment = () => count += 1;
-  const decrement = () => count -= 1;
-  const reset = () => count = 0;
+  const increment = () => (count += 1);
+  const decrement = () => (count -= 1);
+  const reset = () => (count = 0);
 </script>
 
 <div class="bg-gray-200 p-6 rounded-md shadow-md">
   <p class="text-xl font-semibold mb-4">Current count: {count}</p>
 
   <div class="flex gap-4">
-    <Button
-      className="bg-blue-500 text-white hover:bg-blue-600"
-      onClick={increment}
-      textContent="Increment"/>
+    <Button className="bg-blue-500 text-white hover:bg-blue-600" onClick={increment}>
+      Increment
+    </Button>
 
-    <Button
-      className="bg-orange-500 text-white hover:bg-orange-600"
-      onClick={decrement}
-      textContent="Decrement"/>
-    
-    <Button
-      className="bg-gray-500 text-white hover:bg-gray-600"
-      onClick={reset}
-      textContent="Reset"/>
+    <Button className="bg-orange-500 text-white hover:bg-orange-600" onClick={decrement}>
+      Decrement
+    </Button>
+
+    <Button className="bg-gray-500 text-white hover:bg-gray-600" onClick={reset}>Reset</Button>
   </div>
 </div>
