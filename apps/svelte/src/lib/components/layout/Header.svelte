ee67024d488e75770e96ae97a6addad27e44b557
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2023-12-21 16:16:27 +0100

    fix(svelte): link styles

--- apps/svelte/src/lib/components/layout/Header.svelte
+++ apps/svelte/src/lib/components/layout/Header.svelte
@@ -95,13 +95,13 @@
             <li>
               <a
                 href={route.path}
-                class="block py-2 pr-4 lg:p-0"
+                class="block py-2 pr-4 pl-3 lg:p-0"
                 use:link
                 use:active={{
                   className:
-                    'pl-3 text-white rounded bg-blue-700 lg:bg-transparent lg:text-blue-700 dark:text-white',
+                    'text-white rounded bg-blue-700 lg:bg-transparent lg:text-blue-700 dark:text-white',
                   inactiveClassName:
-                    'pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700',
+                    'text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-blue-700 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700',
                 }}
                 aria-current={route.path === $location ? 'page' : null}
               >
