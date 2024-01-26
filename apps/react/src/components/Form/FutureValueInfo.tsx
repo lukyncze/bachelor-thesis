ba86cd2a1ddf1bf4d60c3a7a528c3a7fd25b981d
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2024-01-26 18:00:42 +0100

    style(react): investment form

--- apps/react/src/components/Form/FutureValueInfo.tsx
+++ apps/react/src/components/Form/FutureValueInfo.tsx
@@ -9,7 +9,7 @@ const getLocalizedNumber = (value: number): string => `${value.toLocaleString('d
 
 function FutureValueInfo({children, futureValue}: FutureValueInfoProps) {
   return (
-    <div className="w-1/2 pr-4">
+    <div className="p-1 sm:w-1/2">
       <p className="text-xl font-semibold mb-2 text-gray-800">{children}:</p>
       <p className="text-5xl font-bold">{getLocalizedNumber(futureValue)}</p>
     </div>
--- apps/react/src/components/Form/FutureValuesInfo.tsx
+++ apps/react/src/components/Form/FutureValuesInfo.tsx
@@ -5,7 +5,7 @@ type PresentFutureValuesProps = FutureValues;
 
 function FutureValuesInfo({futureValueOfSavings, futureValueOfSP500}: PresentFutureValuesProps) {
   return (
-    <div className="flex mt-4 bg-blue-300 p-6 rounded-lg shadow-md">
+    <div className="space-y-6 mt-4 bg-blue-300 p-6 rounded-lg shadow-md sm:flex sm:space-y-0">
       <FutureValueInfo futureValue={futureValueOfSavings}>
         Future value on savings account
       </FutureValueInfo>
