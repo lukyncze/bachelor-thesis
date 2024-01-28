57b6b1553afa346935d11d9b2953e02ecfbc34bb
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2024-01-28 17:21:02 +0100

    refactor(react): future value info remove : char

--- apps/react/src/components/Form/FutureValueInfo.tsx
+++ apps/react/src/components/Form/FutureValueInfo.tsx
@@ -10,7 +10,7 @@ const getLocalizedNumber = (value: number): string => `${value.toLocaleString('d
 function FutureValueInfo({children, futureValue}: FutureValueInfoProps) {
   return (
     <div className="p-1 sm:w-1/2">
-      <p className="text-xl font-semibold mb-2 text-gray-800">{children}:</p>
+      <p className="text-xl font-semibold mb-2 text-gray-800">{children}</p>
       <p className="text-5xl font-bold">{getLocalizedNumber(futureValue)}</p>
     </div>
   );
