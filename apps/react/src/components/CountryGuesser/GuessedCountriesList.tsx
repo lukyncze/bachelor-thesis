fa4ad9249087a1f8c50bc15211b5273b300d3d8a
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2024-02-08 21:17:54 +0100

    feat(react): add empty state for GuessedCountriesList + style

--- apps/react/src/components/CountryGuesser/GuessedCountriesList.tsx
+++ apps/react/src/components/CountryGuesser/GuessedCountriesList.tsx
@@ -13,14 +13,18 @@ function GuessedCountriesList({countries, guessedCountries}: GuessedCountriesLis
     <div>
       <h2 className="text-xl font-bold mb-3">Guessed countries</h2>
 
-      <div className="flex flex-col w-96">
-        <ul className="space-y-2">
-          {guessedCountriesWithFlag.map((country, index) => (
-            <li key={index} className="rounded-md bg-gray-100 p-1">
-              {index + 1}. {country.name} {country.flag}
-            </li>
-          ))}
-        </ul>
+      <div className="flex flex-col w-[28rem]">
+        {guessedCountriesWithFlag.length > 0 ? (
+          <ul className="space-y-2">
+            {guessedCountriesWithFlag.map((country, index) => (
+              <li key={index} className="rounded-md bg-gray-100 p-1.5">
+                {index + 1}. {country.name} {country.flag}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No country guessed yet. Please, take a guess!</p>
+        )}
       </div>
     </div>
   );
