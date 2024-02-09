8860e65f5f921c42bf8b454b9d54d45f54d1ec21
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2024-02-09 20:10:23 +0100

    refactor(react): useCountries - Country interface, API call

--- apps/react/src/components/CountryGuesser/useCountries.ts
+++ apps/react/src/components/CountryGuesser/useCountries.ts
@@ -2,20 +2,17 @@ import {useEffect, useRef, useState} from 'react';
 import {mockedData} from './mockedData';
 
 export interface Country {
-  flags: Flags;
   name: Name;
-  independent: boolean;
-  currencies: Currencies;
-  capital: ReadonlyArray<string>;
-  region: string;
-  subregion: string;
-  languages: Languages;
-  latlng: ReadonlyArray<number>;
-  landlocked: boolean;
-  borders: ReadonlyArray<string>;
   area: number;
-  flag: string;
   population: number;
+  landlocked: boolean;
+  region: string;
+  languages: Languages;
+  capital: ReadonlyArray<string>;
+  borders: ReadonlyArray<string>;
+  flags: Flags;
+  flag: string;
+  latlng: ReadonlyArray<number>;
 }
 
 interface Flags {
@@ -27,13 +24,6 @@ interface Flags {
 interface Name {
   common: string;
   official: string;
-}
-
-interface Currencies {
-  [key: string]: {
-    name: string;
-    symbol: string;
-  };
 }
 
 interface Languages {
@@ -53,7 +43,7 @@ function useCountries() {
 
       try {
         // const response = await fetch(
-        //   'https://restcountries.com/v3.1/all?fields=name,flags,region,subregion,population,capitals,currencies,independent,landlocked,area,capital,languages,flag,borders,latlng',
+        //   'https://restcountries.com/v3.1/all?fields=name,area,population,landlocked,region,languages,capital,borders,flags,flag,latlng',
         //   {signal: abortControllerRef.current?.signal},
         // );
         // const data = await response.json();
