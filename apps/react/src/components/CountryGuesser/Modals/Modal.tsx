81e8d085209c3f5bad59646b00d2e1fe8e912feb
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2024-02-09 19:14:06 +0100

    style(react): base Modal adjustments

--- apps/react/src/components/CountryGuesser/Modals/Modal.tsx
+++ apps/react/src/components/CountryGuesser/Modals/Modal.tsx
@@ -1,4 +1,6 @@
 import {ReactNode} from 'react';
+import Button from '../../Button/Button';
+import {closeIcon} from '../../icons/closeIcon';
 
 interface ModalProps {
   isOpen: boolean;
@@ -12,58 +14,41 @@ function Modal({isOpen, handleClose, title, children}: ModalProps) {
     return null;
   }
 
-  // TODO: Refactor modal
   return (
     <div
       id="static-modal"
       data-modal-backdrop="static"
       tabIndex={-1}
       aria-hidden="true"
-      className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      className="overflow-y-auto overflow-x-hidden flex fixed items-center justify-center inset-0 z-50 w-full h-[calc(100%-1rem)] max-h-full"
     >
-      <div className="relative p-4 w-full max-w-2xl max-h-full">
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+      <div className="relative p-4 w-full max-w-3xl max-h-full">
+        <div className="relative border bg-gray-200 border-gray-400 rounded-lg shadow dark:bg-gray-700">
+          <div className="flex items-center justify-between p-3 md:px-5 md:py-3 border-b border-gray-300 rounded-t dark:border-gray-600">
             <h3 className="text-xl font-semibold text-gray-900 dark:text-white">{title}</h3>
 
             <button
-              type="button"
-              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
               data-modal-hide="static-modal"
+              className="text-gray-800 bg-transparent hover:bg-gray-300 hover:text-gray-900 rounded-lg text-sm w-10 h-10 ms-auto inline-flex justify-center items-center dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
               onClick={handleClose}
             >
-              <svg
-                className="w-3 h-3"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 14"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                />
-              </svg>
+              {closeIcon}
               <span className="sr-only">Close modal</span>
             </button>
           </div>
 
           <div className="p-4 md:p-5 space-y-4">
-            <p className="text-lg leading-relaxed text-gray-500 dark:text-gray-400">{children}</p>
+            <p className="text-lg leading-relaxed text-gray-600 dark:text-gray-300">{children}</p>
           </div>
 
-          <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-            <button
+          <div className="flex items-center p-3 md:px-5 md:py-3 border-t border-gray-300 rounded-b dark:border-gray-600">
+            <Button
               data-modal-hide="static-modal"
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-sm text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               onClick={handleClose}
             >
-              Restart
-            </button>
+              Restart the game
+            </Button>
           </div>
         </div>
       </div>
