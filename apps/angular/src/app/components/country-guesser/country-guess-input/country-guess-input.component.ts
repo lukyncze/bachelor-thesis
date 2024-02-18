244def5beec7710661f6ca2a19c2d1961515d4ca
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2024-02-18 13:11:37 +0100

    refactor(angular): CountryGuessInput - move currectGuess state, use handlers

--- apps/angular/src/app/components/country-guesser/country-guess-input/country-guess-input.component.ts
+++ apps/angular/src/app/components/country-guesser/country-guess-input/country-guess-input.component.ts
@@ -1,6 +1,7 @@
+import {CommonModule} from '@angular/common';
 import {Component, EventEmitter, Input, OnChanges, Output} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {Countries, Country} from '../country';
+import {Countries} from '../country';
 import {GuessedCountries} from '../country-guesser.component';
 
 const countryHintsCount = 8;
@@ -9,73 +10,80 @@ const countryHintsCount = 8;
   selector: 'country-guess-input',
   standalone: true,
   templateUrl: './country-guess-input.component.html',
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
 })
 export class CountryGuessInputComponent implements OnChanges {
+  private countriesWithoutAlreadyGuessed: Countries = [];
+  protected filteredCountries: Countries = [];
+  protected currentGuess = '';
   protected isOpen = false;
   protected isValidGuess = false;
   protected selectedGuessIndex = 0;
-  protected filteredCountries: Countries = [];
 
   @Input({required: true}) public countries: Countries = [];
-  @Input({required: true}) public currentGuess = '';
   @Input({required: true}) public guessedCountries: GuessedCountries = [];
 
-  @Output() public setCurrentGuess = new EventEmitter<string>();
-  @Output() public evaluateGuessAndUpdateState = new EventEmitter<void>();
+  @Output() public evaluateGuessAndUpdateState = new EventEmitter<string>();
 
   public ngOnChanges(): void {
-    this.updateFilteredCountries();
+    this.countriesWithoutAlreadyGuessed = this.countries.filter(
+      country => !this.guessedCountries.includes(country.name.common),
+    );
+    this.filteredCountries = this.countriesWithoutAlreadyGuessed.slice(0, countryHintsCount);
   }
 
-  protected handleChangeSelectedGuess(guess: string): void {
-    this.setCurrentGuess.emit(guess);
-    this.selectedGuessIndex = 0;
+  protected handleGuessButtonClick(): void {
+    if (this.isValidGuess) {
+      this.evaluateGuessAndUpdateState.emit(this.currentGuess);
+    }
+
+    this.updateFilteredCountries('');
     this.isOpen = false;
   }
 
-  protected handleInputChange(): void {
-    const formattedGuess = this.convertToFormattedGuess(this.currentGuess);
-    this.setCurrentGuess.emit(formattedGuess);
+  protected handleChangeSelectedGuess(guessedCountry: string): void {
+    this.selectedGuessIndex = 0;
+    this.updateFilteredCountries(guessedCountry);
+    this.isOpen = false;
   }
 
-  protected handleKeyDown({key}: KeyboardEvent): void {
+  protected handleInputChange(value: string): void {
+    const formattedGuess = this.convertToFormattedGuess(value);
+    this.updateFilteredCountries(formattedGuess);
+  }
+
+  protected handleKeyDown(event: KeyboardEvent): void {
     if (!this.isOpen) return;
 
-    if (key === 'ArrowDown') {
-      this.changeSelectedGuessIndex(1);
-    } else if (key === 'ArrowUp') {
+    if (event.key === 'ArrowUp') {
       this.changeSelectedGuessIndex(-1);
-    } else if (key === 'Enter' && this.filteredCountries.length > 0) {
+      event.preventDefault();
+    } else if (event.key === 'ArrowDown') {
+      this.changeSelectedGuessIndex(1);
+      event.preventDefault();
+    } else if (event.key === 'Enter' && this.filteredCountries.length > 0) {
       this.handleChangeSelectedGuess(this.filteredCountries[this.selectedGuessIndex].name.common);
-    } else if (key === 'Escape') {
+    } else if (event.key === 'Escape') {
       this.isOpen = false;
     }
   }
 
-  private updateFilteredCountries(): void {
-    const countriesWithoutAlreadyGuessed = this.countries.filter(country =>
-      this.filterOutAlreadyGuessedCountries(country),
-    );
-    const filteredCountries = countriesWithoutAlreadyGuessed.filter(country =>
-      this.filterOutSearchByUserGuess(country),
-    );
+  private updateFilteredCountries(guessedCountry: string): void {
+    this.currentGuess = guessedCountry;
 
-    this.filteredCountries = filteredCountries.slice(0, countryHintsCount);
-    this.isValidGuess = !!filteredCountries.find(country => this.searchForExactCountry(country));
+    const filteredCountries = this.getFilteredCountries();
+    const isValidGuess = !!filteredCountries.find(({name}) => name.common === this.currentGuess);
+
+    this.isValidGuess = isValidGuess;
+    this.filteredCountries = filteredCountries;
     this.clampSelectedGuessIndex();
   }
 
-  private filterOutAlreadyGuessedCountries(country: Country): boolean {
-    return !this.guessedCountries.includes(country.name.common);
-  }
-
-  private filterOutSearchByUserGuess(country: Country): boolean {
-    return country.name.common.toLowerCase().includes(this.currentGuess.toLowerCase());
-  }
-
-  private searchForExactCountry(country: Country): boolean {
-    return country.name.common === this.currentGuess;
+  private getFilteredCountries() {
+    const filteredCountries = this.countriesWithoutAlreadyGuessed.filter(country =>
+      country.name.common.toLowerCase().includes(this.currentGuess.toLowerCase()),
+    );
+    return filteredCountries.slice(0, countryHintsCount);
   }
 
   private clampSelectedGuessIndex(): void {
