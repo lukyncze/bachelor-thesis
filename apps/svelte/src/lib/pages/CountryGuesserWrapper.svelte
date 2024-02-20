67e7f6fbc87fb19620bcfafddb38ea221f459f7c
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2024-02-20 17:48:19 +0100

    feat(svelte): fetch countries data on component mount

--- apps/svelte/src/lib/pages/CountryGuesserWrapper.svelte
+++ apps/svelte/src/lib/pages/CountryGuesserWrapper.svelte
@@ -1,16 +1,51 @@
 <script lang="ts">
+  import {CanceledError} from 'axios';
   import {onMount} from 'svelte';
+  import {getAllCountries} from '../components/country-guesser/api/getAllCountries';
+  import {type Countries} from '../components/country-guesser/country';
   import CountryGuesser from '../components/country-guesser/CountryGuesser.svelte';
   import ErrorAlert from '../components/country-guesser/ErrorAlert.svelte';
   import LoadingSkeleton from '../components/country-guesser/LoadingSkeleton.svelte';
-  import {mockedData} from '../components/country-guesser/mockedData';
 
-  let countries = mockedData.toSorted((a, b) => a.name.common.localeCompare(b.name.common));
+  let countries: Countries = [];
+  let isLoading = false;
   let error: Error | null = null;
+  let abortController: AbortController | null = null;
 
-  onMount(() => {
-    // fetch data
-  });
+  onMount(() => fetchCountriesData());
+
+  const fetchCountriesData = async (): Promise<void> => {
+    isLoading = true;
+    abortController?.abort();
+    abortController = new AbortController();
+
+    try {
+      const options = {signal: abortController.signal};
+      const response = await getAllCountries(options);
+
+      if (response.code === 'error') {
+        if (response.error instanceof CanceledError) return;
+
+        throw new Error(
+          `There was an error with getting the countries data (${response.error.message}). Please reload the page.`
+        );
+      }
+
+      if (response.data.length === 0) {
+        throw new Error('There are no countries to guess. Please try again later.');
+      }
+
+      countries = getSortedCountriesByName(response.data);
+    } catch (err: any) {
+      error = err;
+    } finally {
+      isLoading = false;
+    }
+  };
+
+  const getSortedCountriesByName = (countries: Countries): Countries => {
+    return countries.toSorted((a, b) => a.name.common.localeCompare(b.name.common));
+  };
 </script>
 
 {#if error}
