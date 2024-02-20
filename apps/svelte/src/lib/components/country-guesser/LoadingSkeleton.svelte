aef96261d80d88c327a6b77a0c54ae8a8f0b73d2
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2024-02-20 17:30:34 +0100

    feat(svelte): add ErrorAlert + LoadingSkeleton

---
+++ apps/svelte/src/lib/components/country-guesser/LoadingSkeleton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  const items = Array.from({length: 6});
+</script>
+
+<div role="status" class="animate-pulse">
+  {#each items as item, index}
+    <div
+      class={`${index === 0 ? '!h-6' : 'h-4'} 
+        bg-gray-300 rounded-full dark:bg-gray-400 w-full mt-4`}
+    ></div>
+  {/each}
+  <span class="sr-only">Loading...</span>
+</div>
--- apps/svelte/src/lib/pages/CountryGuesserWrapper.svelte
+++ apps/svelte/src/lib/pages/CountryGuesserWrapper.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
+  import {onMount} from 'svelte';
   import CountryGuesser from '../components/country-guesser/CountryGuesser.svelte';
+  import ErrorAlert from '../components/country-guesser/ErrorAlert.svelte';
+  import LoadingSkeleton from '../components/country-guesser/LoadingSkeleton.svelte';
   import {mockedData} from '../components/country-guesser/mockedData';
 
   let countries = mockedData.toSorted((a, b) => a.name.common.localeCompare(b.name.common));
-  let error = false;
+  let error: Error | null = null;
+
+  onMount(() => {
+    // fetch data
+  });
 </script>
 
 {#if error}
-  <div class="container flex flex-col justify-center justify-items-center mx-auto">ErrorAlert</div>
+  <div class="container flex flex-col justify-center justify-items-center mx-auto">
+    <ErrorAlert message={error.message} />
+  </div>
 {:else if countries.length > 0}
   <CountryGuesser {countries} />
 {:else}
   <div class="container flex flex-col justify-center justify-items-center mx-auto">
-    LoadingSkeleton
+    <LoadingSkeleton />
   </div>
 {/if}
