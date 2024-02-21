a7c1e7030cc0a7eb1da282dccb6c3d2960f91a8a
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2024-02-21 17:55:19 +0100

    refactor(angular): use ngModel instead of value and input

--- apps/angular/src/app/components/translator/translation-input/translation-input.component.ts
+++ apps/angular/src/app/components/translator/translation-input/translation-input.component.ts
@@ -1,13 +1,14 @@
 import {CommonModule} from '@angular/common';
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {AutosizeTextAreaDirective} from '../autosize-text-area/autosize-text-area.directive';
+import {FormsModule} from '@angular/forms';
 import {CloseIconComponent} from '../../icons/close-icon/close-icon.component';
+import {AutosizeTextAreaDirective} from '../autosize-text-area/autosize-text-area.directive';
 
 @Component({
   selector: 'translation-input',
   standalone: true,
   templateUrl: './translation-input.component.html',
-  imports: [CommonModule, CloseIconComponent, AutosizeTextAreaDirective],
+  imports: [CommonModule, FormsModule, CloseIconComponent, AutosizeTextAreaDirective],
 })
 export class TranslationInputComponent {
   protected maximumCharactersCount = 800;
@@ -16,11 +17,9 @@ export class TranslationInputComponent {
 
   @Output() public setInputText = new EventEmitter<string>();
 
-  protected handleInputChange(event: Event): void {
-    const target = event.target as HTMLTextAreaElement;
-
-    if (target.value.length <= this.maximumCharactersCount) {
-      this.setInputText.emit(target.value);
+  protected handleInputChange(value: string): void {
+    if (value.length <= this.maximumCharactersCount) {
+      this.setInputText.emit(value);
     }
   }
 }
