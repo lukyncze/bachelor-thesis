e5617c5ce54c222e905b06d92c7d273b3dd79ad6
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2024-02-24 17:44:15 +0100

    docs(svelte): add comments

--- apps/svelte/src/lib/components/country-guesser/api/queries.ts
+++ apps/svelte/src/lib/components/country-guesser/api/queries.ts
@@ -2,7 +2,7 @@ import {createQuery} from '@tanstack/svelte-query';
 import {type Countries} from '../country';
 import {getAllCountries} from './getAllCountries';
 
+// Funkce sloužící k získání všech zemí.
 export const useAllCountries = () => {
   return createQuery<Countries>({queryKey: ['allCountriesQuery'], queryFn: getAllCountries});
 };
-
