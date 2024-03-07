08a3c0e2f42c95d7d88d280ccb5a38592c62d7c2
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2024-03-07 20:18:41 +0100

    refactor(svelte): add type to useAllCountries query

--- apps/svelte/src/lib/components/country-guesser/api/queries.ts
+++ apps/svelte/src/lib/components/country-guesser/api/queries.ts
@@ -1,8 +1,8 @@
-import {createQuery} from '@tanstack/svelte-query';
+import {createQuery, type CreateQueryResult} from '@tanstack/svelte-query';
 import {type Countries} from '../country';
 import {getAllCountries} from './getAllCountries';
 
 // Funkce sloužící k získání všech zemí.
-export const useAllCountries = () => {
+export const useAllCountries = (): CreateQueryResult<Countries, Error> => {
   return createQuery<Countries>({queryKey: ['allCountriesQuery'], queryFn: getAllCountries});
 };
