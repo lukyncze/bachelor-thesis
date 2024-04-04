45f8b7a705e787ba6848031a78cfaedfa6af13d9
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2024-04-04 17:11:41 +0200

    chore(vue): remove project

--- apps/vue/src/components/Dropdown/dropdownSize.ts
+++
@@ -1,7 +0,0 @@
-export type DropdownSize = 'sm' | 'md' | 'lg';
-
-export const dropdownSize: Record<DropdownSize, string> = {
-  sm: 'py-1.5 px-2 text-xs',
-  md: 'py-2 px-3 text-sm',
-  lg: 'py-3 px-5 text-base',
-}
